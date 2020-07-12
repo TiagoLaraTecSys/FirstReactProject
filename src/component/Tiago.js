f80fd53768421a862ffaeb0814cf2c13b4a80354
tiago de lara ribeiro <laratecsys@gmail.com>
2020-07-12 13:39:07 -0300

    Criando componente com propriedades e customizando elas

--- src/component/Tiago.js
+++ src/component/Tiago.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-
+import User from './User';
+import photo from '../userfoto.png'
 class Tiago extends Component{
     render(){
         return(
             <div>
                 <p>Meu componente chamado Tiago</p>
+                <User name="Tiago" photo={photo}></User>
+                <User name="Andrea"></User>
             </div>
         );
     }
