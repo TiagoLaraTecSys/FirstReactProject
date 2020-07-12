de11b88ef2e6532e311cb80c1f61be142a1551e3
tiago de lara ribeiro <laratecsys@gmail.com>
2020-07-12 14:04:14 -0300

    Mudando estado de um input através de um metodo change

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-        name: "Tiago Ribeiro"
+        name: "Tiago Ribeiro",
+        email: "laratecsys@gmail.com",
+
     }
 
     this.changestate = this.changestate.bind(this);
     this.resetstate = this.resetstate.bind(this);
+    this.changeinput = this.changeinput.bind(this);
 }
 
 changestate(){
@@ -26,6 +29,15 @@ resetstate(){
     name: "Tiago Ribeiro"
   })
 }
+
+changeinput(event){
+  let target = event.target
+  let index  = target.name
+  this.setState({
+    [index]: target.value
+  })
+}
+
 render() {
   return (
     <div className="App">
@@ -33,7 +45,20 @@ render() {
         <button onClick={this.changestate}>Mudar ESTADO</button>
         <button onClick={this.resetstate}>Resetar ESTADO</button>
       </div>
-      {this.state.name}
+      <div>
+        <form>
+          <label>Nome
+            <input  type="text" name="name" value={this.state.name} onChange={this.changeinput}>
+            </input>
+          </label>
+          <label>Email 
+            <input type="email" name="email" value={this.state.email} onChange={this.changeinput}>
+            </input>            
+          </label>
+        </form>
+        {this.state.name} - {this.state.email}
+      </div>
+      
     </div>
   );
 }
