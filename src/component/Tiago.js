26fb1ce49f434ac32297390f92008e1b78038aba
tiago de lara ribeiro <laratecsys@gmail.com>
2020-07-12 13:26:32 -0300

    Criando um componente com React

---
+++ src/component/Tiago.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tiago extends Component{
+    render(){
+        return(
+            <div>
+                <p>Meu componente chamado Tiago</p>
+            </div>
+        );
+    }
+}
+
+export default Tiago;
